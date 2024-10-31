ac48fb10fcbc4160f3831113e92651c935f96b1a
CesarXiu <cesarxiu504@gmail.com>
2024-10-30 19:06:29 -0500

    login tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,64 @@
 @extends('layout.layout')
 
 @section('content')
-<form action="{{route('login.post')}}" method="POST">
+<div class="h-screen w-full bg-gradient-to-b from-white to-purple-300 flex flex-col items-center justify-start">
+        <div class="flex flex-col items-center">
+            <div class="mb-8">
+                <img src="/Logo.png" alt="Imagen" style="width: auto; height: 80%;">
+            </div>
+        </div>
+        <div class="w-full max-w-md bg-beige p-6 rounded-lg shadow-md mx-auto bg-gray-100">
+            <h1 class="text-6xl font-serif text-gray-700 mb-8 text-center">
+                SIGA
+            </h1>
+            <form action="{{route('login.post')}}" method="POST" class="space-y-4">
+            @csrf
+            @error('success')
+                <div class="success">
+                    {{$message}}
+                </div>
+                <br><br>
+            @enderror
+            @error('error')
+                <div class="error">
+                    {{$message}}
+                </div>
+                <br><br>
+            @enderror
+                <div>
+                    <label for="email" class="block text-sm font-medium text-gray-700">
+                        Correo Electrónico
+                    </label>
+                    <input type="email" name="email" id="email" required
+                        value="{{old('email')}}"
+                        class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        <div class="error">
+                    @error('email')
+                        {{$message}}
+                    @enderror
+                </div>
+                    </div>
+                <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">
+                        Contraseña
+                    </label>
+                    <input type="password" name="password" id="password" required
+                        class="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" />
+                        <div class="error">
+                    @error('password')
+                        {{$message}}
+                    @enderror
+                </div>    
+                </div>
+                <button type="submit"
+                    class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded-md focus:outline-none focus:shadow-outline">
+                    Ingresar
+                </button>
+            </form>
+            Don't have an account? <a href="{{route('register')}}">Register here</a>
+        </div>
+    </div>
+<!--<form action="{{route('login.post')}}" method="POST">
     @csrf
     @error('success')
         <div class="success">
@@ -40,5 +97,5 @@
     <button type="submit">Login</button>
     <br>
     Don't have an account? <a href="{{route('register')}}">Register here</a>
-</form>
+</form>-->
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
+    
+
 </head>
 
 <body>
@@ -16,13 +19,13 @@
             margin: 0;
             box-sizing: border-box;
         }
-        .container {
+        /*.container {
             width: 100vw;
             height: 100vh;
             display: flex;
             justify-content: center;
             margin: auto;
-        }
+        }*/
         .container > form {
             margin: auto;
         }
@@ -38,7 +41,7 @@
             color: green;
         }
     </style>
-    <div class="container">
+    <div>
         @yield('content')
     </div>
 </body>
