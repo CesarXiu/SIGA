46de585b164c6da05765f78aa7163a65dc9c6b9e
CesarXiu <131189969+CesarXiu@users.noreply.github.com>
2025-01-03 20:07:29 -0500

    Documentacion traits

--- app/Http/Traits/ApiTraits.php
+++ app/Http/Traits/ApiTraits.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Http\Traits;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
 /*
     TODOS ESTOS TRAITS SON PARA UTILIZARLOS EN LOS CONTROLADORES DE API
     SON CARGADOS DESDE EL MODELO BASE Y SON HEREDADOS A TODOS LOS DEMAS MODELOS QUE HEREDEN DE ESTE
@@ -46,6 +45,19 @@ trait ApiTraits{
         $query->with($relations);
     }
 
+    /**
+     * Filtra la consulta basada en los filtros permitidos y los valores proporcionados en la solicitud.
+     *
+     * @param Builder $query La consulta de Eloquent Builder que se va a filtrar.
+     * @return void
+     *
+     * La función verifica si hay filtros permitidos definidos en la propiedad $allowFilter del modelo
+     * y si hay filtros proporcionados en la solicitud. Si alguno de estos está vacío, la función retorna sin hacer nada.
+     *
+     * Luego, convierte la propiedad $allowFilter en una colección y obtiene los filtros de la solicitud.
+     * Recorre cada filtro proporcionado en la solicitud y verifica si está en la lista de filtros permitidos.
+     * Si el filtro está permitido, agrega una condición "where" a la consulta para ese filtro y su valor correspondiente.
+     */
     public function scopeFiltered(Builder $query){
         if(empty($this->allowFilter) || empty(request('filter'))){
             return;
@@ -59,18 +71,34 @@ trait ApiTraits{
         }
     }
 
+    /**
+     * Método de alcance para ordenar los resultados de una consulta.
+     *
+     * Este método permite ordenar los resultados de una consulta basada en los parámetros
+     * de ordenamiento proporcionados en la solicitud HTTP. Si no se especifican parámetros
+     * de ordenamiento o no se permiten los campos de ordenamiento, no se realiza ninguna acción.
+     *
+     * @param Builder $query La consulta que se va a ordenar.
+     * @return void
+     */
     public function scopeSorted(Builder $query){
+        // Verifica si no hay campos permitidos para ordenar o si no se especifica el parámetro 'sort' en la solicitud.
         if(empty($this->allowSort) || empty(request('sort'))){
             return;
         }
+        // Convierte los campos permitidos para ordenar en una colección.
         $allowSort = collect($this->allowSort);
+        // Divide los parámetros de ordenamiento proporcionados en la solicitud en un array.
         $sorts = explode(',', request('sort'));
+        // Itera sobre cada parámetro de ordenamiento.
         foreach ($sorts as $sort) {
-            $direction = 'asc';
+            $direction = 'asc'; // Dirección predeterminada de ordenamiento.
+            // Si el parámetro de ordenamiento comienza con '-', se establece la dirección a 'desc'.
             if(substr($sort, 0, 1) == '-'){
                 $direction = 'desc';
-                $sort = substr($sort, 1);
+                $sort = substr($sort, 1); // Elimina el '-' del nombre del campo.
             }
+            // Si el campo de ordenamiento está permitido, se agrega a la consulta con la dirección especificada.
             if($allowSort->contains($sort)){
                 $query->orderBy($sort, $direction);
             }
--- app/Http/Traits/UtilTraits.php
+++ app/Http/Traits/UtilTraits.php
@@ -1,20 +1,45 @@
 <?php
 namespace App\Http\Traits;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
-
+/**
+ * Trait UtilTraits
+ *
+ * Este trait proporciona métodos para manejar colecciones de recursos y recursos individuales.
+ * 
+ * Métodos:
+ * - resourceCollection($data): Devuelve una colección de recursos en un formato específico.
+ * - resource(): Devuelve un recurso individual en un formato específico.
+ * 
+ * Métodos abstractos:
+ * - getResourceClass(): Debe ser implementado por la clase que use este trait para devolver la clase del recurso.
+ */
 trait UtilTraits
 {
+    /**
+     * Devuelve una colección de recursos en un formato específico.
+     *
+     * @param mixed $data Los datos que se convertirán en una colección de recursos.
+     * @return array Un array con la colección de recursos.
+     */
     public static function resourceCollection($data)
     {
         return ["data" => static::getResourceClass()::collection($data)];
     }
 
+    /**
+     * Devuelve un recurso individual en un formato específico.
+     *
+     * @return array Un array con el recurso individual.
+     */
     public function resource()
     {
         return ["data" => static::getResourceClass()::make($this)];
     }
 
+    /**
+     * Debe ser implementado por la clase que use este trait para devolver la clase del recurso.
+     *
+     * @return string El nombre de la clase del recurso.
+     */
     abstract protected static function getResourceClass();
 }
